4bdf2879b0716a33988a500cb9076f5d4607d6d2
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-27 17:31:34 +0530

    done with model imp and blogs inhance ui littlt bit

--- app/src/main/java/com/example/agrotech/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/agrotech/Fragments/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.agrotech.R
 
 import com.example.agrotech.databinding.HomeFragmentBinding
 
@@ -15,6 +17,20 @@ class HomeFragment: BaseFragment(){
         savedInstanceState: Bundle?
     ): View? {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnhmgrdn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_blogHomeGarden)
+        }
+        binding.btncashcrp.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_cashCropFragment)
+        }
+
+        binding.btnpredict.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_camFragment2)
+        }
     }
 
 
