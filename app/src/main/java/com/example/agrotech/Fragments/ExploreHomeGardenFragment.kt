f03e0a6f97144737848f5dc9b5619c6c73fcbd13
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-28 01:58:53 +0530

    integrated fertilizer model sucessfully added 2-3 more activied

---
+++ app/src/main/java/com/example/agrotech/Fragments/ExploreHomeGardenFragment.kt
@@ -0,0 +1,18 @@
+package com.example.agrotech.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.agrotech.databinding.ExploreHomeGardenFragmentBinding
+
+class ExploreHomeGardenFragment :BaseFragment() {
+    private val binding by lazy { ExploreHomeGardenFragmentBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/agrotech/Fragments/FertilizerResultFragment.kt
@@ -0,0 +1,24 @@
+package com.example.agrotech.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.agrotech.databinding.FertilizerResultFragmentBinding
+
+class FertilizerResultFragment:BaseFragment() {
+    private val binding by lazy { FertilizerResultFragmentBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textferresult.text = FertilizerFragment.result
+    }
+}
